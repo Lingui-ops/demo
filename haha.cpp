88f24cb5b4e88b210307118a36ef577c00d3d930
LZY <te1900@foxmail.com>
2021-03-28 15:17:50 +0800

    233

--- haha.cpp
+++ haha.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
-	printf("hello world");
-
-
-
-
-	return 0;
+    printf("hello world");
+    int sum = 0;
+    for (int i = 0; i < 10; ++i){
+        sum += i;
+    }
+    printf("%d\n", sum);
+    return 0;
 }
